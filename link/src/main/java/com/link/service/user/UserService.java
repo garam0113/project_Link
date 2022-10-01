d78279a65e5f3a5a94cd4f0d0248908c46605d0f
bitcamp <bitcamp@DESKTOP-B951KAA>
2022-10-01 17:06:20 +0900

    User_jeongMin:2022.10.01.17:04 UserMepper작성 완료 DAO작성 완료 Service작성 완료 User 도메인객체 작성 완료

--- link/src/main/java/com/link/service/user/UserService.java
+++ link/src/main/java/com/link/service/user/UserService.java
@@ -1,5 +1,46 @@
 package com.link.service.user;
 
+import java.util.Map;
+
+import com.link.common.Search;
+import com.link.service.domain.User;
+
 public interface UserService {
 
+	public void addUser(User user) throws Exception;
+	
+	public void addSnsUser(User user) throws Exception;
+	
+	public void addBlockUser(User user) throws Exception;
+	
+	public User getUser(String userId) throws Exception;
+	
+	public User getId(String name, int rrn) throws Exception;
+
+	public void updateUser(User user) throws Exception;
+	
+	public void updateProfile(User user) throws Exception;
+	
+	public void updatePhoneNo(User user) throws Exception;
+	
+	public void updatePassword(User user) throws Exception;
+
+	public void updateBlockUser(User user) throws Exception;
+	
+	public void logout(User user) throws Exception;
+	
+	//public void updateBlockUser(Block block) throws Exception;
+		
+	public void deleteUser(User user) throws Exception;
+	
+	public boolean checkDuplication(String userId) throws Exception;
+	
+	public void sendSMS(String phoneNo, String cerNo);
+	
+	public Map<String, Object> getUserList(Search search) throws Exception;
+	
+	public Map<String, Object> getStopList(Search search) throws Exception;
+
+	public Map<String, Object> getForeverStopList(Search search) throws Exception;
+	
 }
--- link/src/main/java/com/link/service/user/impl/UserDAOImpl.java
+++ link/src/main/java/com/link/service/user/impl/UserDAOImpl.java
@@ -1,11 +1,90 @@
 package com.link.service.user.impl;
 
+import java.util.List;
+
+import com.link.common.Search;
+import com.link.service.domain.User;
 import com.link.service.user.UserDAO;
 
 public class UserDAOImpl implements UserDAO {
 
-	public UserDAOImpl() {
-		// TODO Auto-generated constructor stub
+	@Override
+	public void addUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
+
+	@Override
+	public void addSnsUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addBlockUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User getUser(String userId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getId(String name, int rrn) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateProfile(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePhoneNo(int phone) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePassword(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBlockUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logout(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> getUserList(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
