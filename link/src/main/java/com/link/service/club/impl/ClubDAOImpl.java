d37c12bace4109166c8720b1f0a9ab8849bd9b99
bitcamp <bitcamp@DESKTOP-C0OEEVP>
2022-10-04 19:22:13 +0900

    안상혁 : 2022.10.04 19:21 DAO Service 완성

--- link/src/main/java/com/link/service/club/impl/ClubDAOImpl.java
+++ link/src/main/java/com/link/service/club/impl/ClubDAOImpl.java
@@ -31,87 +31,133 @@ public class ClubDAOImpl implements ClubDAO {
 
 	
 	//Method
+	@Override
 	public void addClub(Club club) throws Exception {
 		sqlSession.insert("ClubMapper.addClub",club);
 	}
 	
-	public void getClub(int clubNo) throws Exception {
-		sqlSession.selectOne("ClubMapper.getClub",clubNo);
+	@Override
+	public Club getClub(int clubNo) throws Exception {
+		return sqlSession.selectOne("ClubMapper.getClub",clubNo);
 	}
 	
+	@Override
 	public List<Club> getClubList(Search search) throws Exception {
-		return null;
+		return sqlSession.selectList("ClubMapper.getClubList",search);
 	}
 	
-	public void deleteClub(Club club) throws Exception {
-		sqlSession.delete("ClubMapper.deleteClub",club);
+	@Override
+	public void deleteClub(int clubNo) throws Exception {
+		sqlSession.delete("ClubMapper.deleteClub",clubNo);
 	}
 	
+	@Override
 	public void updateClub(Club club) throws Exception {
 		sqlSession.update("ClubMapper.updateClub",club);
 	}
 	
+	@Override
 	public void addApprovalCondition(ClubUser clubUser) throws Exception {
 		sqlSession.insert("ClubMapper.addApprovalCondition",clubUser);
 	}
 	
+	@Override
 	public List<ClubUser> getApprovalConditionList(Search search) throws Exception {
-		return null;
+		return sqlSession.selectList("ClubMapper.getApprovalConditionList",search);
 	}
 	
+	@Override
 	public void updateApprovalCondition(ClubUser clubUser) throws Exception {
 		sqlSession.update("ClubMapper.updateApprovalCondition",clubUser);
 	}
 	
+	@Override
 	public List<ClubUser> getClubMemberList(Search search) throws Exception {
-		return null;
+		return sqlSession.selectList("ClubMapper.getClubMemberList",search);
 	}
 	
-	public void deleteClubMember(ClubUser clubUser) throws Exception {
-		sqlSession.delete("ClubMapper.deleteClubMember",clubUser);
+	@Override
+	public void deleteClubMember(int clubUserNo) throws Exception {
+		sqlSession.delete("ClubMapper.deleteClubMember",clubUserNo);
 	}
 	
+	@Override
 	public void updateMemberRole(ClubUser clubUser) throws Exception {
 		sqlSession.update("ClubMapper.updateMemberRole",clubUser);
 	}
 	
+	@Override
 	public void deleteApprovalCondition(ClubUser clubUser) throws Exception {
 		sqlSession.delete("ClubMapper.deleteApprovalCondition",clubUser);		
 	}
 	
+	@Override
 	public void addMeeting(Meeting meeting) throws Exception {
 		sqlSession.insert("ClubMapper.addMeeting",meeting); 
 	}
 	
+	@Override
 	public List<Meeting> getMeetingList(Search search) throws Exception {
-		return null;
+		return sqlSession.selectList("ClubMapper.getMeetingList",search);
 	}
 	
+	@Override
 	public Meeting getMeeting(int meetingNo) throws Exception {
-		return null;
+		return sqlSession.selectOne("ClubMapper.getMeeting",meetingNo);
 	}
 	
+	@Override
 	public void updateMeeting(Meeting meeting) throws Exception {
 		sqlSession.update("ClubMapper.updateMeeting",meeting);
 	}
 	
+	@Override
 	public void deleteMeeting(Meeting meeting) throws Exception {
 		sqlSession.delete("ClubMapper.deleteMeeting",meeting);
 	}
 	
+	@Override
 	public void addMeetingMember(Meeting meeting) throws Exception {
 		sqlSession.insert("ClubMapper.addMeetingMember",meeting);
 	}
 	
+	@Override
 	public void deleteMeetingMember(Meeting meeting) throws Exception {
 		sqlSession.delete("ClubMapper.deleteMeetingMember",meeting);
 	}
 	
+	@Override
 	public List<Meeting> getMeetingMemberList(Search search) throws Exception {
-		return null;
+		return sqlSession.selectList("ClubMapper.getMeetingMemberList",search);
 	}
 	
+	@Override
 	public void addMeetingPush(Meeting meeting) throws Exception {
 		
+	}
+	
+	@Override
+	public int getTotalClubCount(Search search) {
+		return sqlSession.selectOne("ClubMapper.getTotalClubCount",search);
+	}
+	
+	@Override
+	public int getTotalApprovalConditionCount(Search search) {
+		return sqlSession.selectOne("ClubMapper.getTotalApprovalConditionCount",search);
+	}
+	
+	@Override
+	public int getTotalClubMemberCount(Search search) {
+		return sqlSession.selectOne("ClubMapper.getTotalClubMemberCount",search);
+	}
+	
+	@Override
+	public int getTotalMeetingCount(Search search) {
+		return sqlSession.selectOne("ClubMapper.getTotalMeetingCount",search);
+	}
+	
+	@Override
+	public int getTotalMeetingMemberCount(Search search) {
+		return sqlSession.selectOne("ClubMapper.getTotalMeetingMemberCount",search);
 	}
 }
---
+++ link/src/test/java/club/ClubServiceTest.java
@@ -0,0 +1,52 @@
+package club;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.link.service.club.ClubDAO;
+import com.link.service.club.ClubService;
+import com.link.service.domain.Club;
+import com.link.service.user.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:config/context-*.xml" })
+public class ClubServiceTest {
+	
+	@Autowired
+	@Qualifier("clubServiceImpl")
+	private ClubService clubService;
+	
+	@Autowired
+	@Qualifier("clubDAO")
+	private ClubDAO clubDAO;
+	
+
+//	@Test
+//	public void testAddClub() throws Exception {
+//		
+//		System.out.println("\n ===================================");
+//		Club club = new Club(5,"user01","모임제목","모임내용","a.jpg",1,10,"모임카테고리","모임영역");
+//		System.out.println("insert 결과 : "+clubService.addClub(club));
+//		System.out.println("delete 결과 : "+clubDAO.deleteClub(club.getClubNo()));
+//				
+//	}
+	
+	@Test
+	public void testGetClub() throws Exception {
+		
+		System.out.println("\n ===========================");
+		Club club = clubService.getClub(2);
+		System.out.println(club);
+		
+		Assert.assertEquals(2, club.getClubNo());
+		
+		System.out.println("========================\n");
+		
+	}
+
+}
