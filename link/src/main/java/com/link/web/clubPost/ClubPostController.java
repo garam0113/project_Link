9645f7e9c65da4fcf816557806dbb09b99250f1f
903-19 <903-19@DESKTOP-4K59SEG>
2022-10-16 19:37:36 +0900

    darae : 2022.10.16 디비저장x 하나의 jsp파일에서 일대다 채팅구현

--- link/src/main/java/com/link/web/clubPost/ClubPostController.java
+++ link/src/main/java/com/link/web/clubPost/ClubPostController.java
@@ -179,10 +179,11 @@ public class ClubPostController {
 	
 	
 	
-	@RequestMapping(value = "/chatRoomList", method = RequestMethod.POST)
-	public String chatClubList() throws Exception {
+	@RequestMapping(value = "/chatRoomList", method = RequestMethod.GET)
+	public String chatClubList(HttpSession session, Model model) throws Exception {
 		System.out.println("/chatClubList : POST : 모임번호를 가지고 모임채팅리스트 화면으로 이동");
-		return "forward:/clubPost/chatRoomList.jsp";
+		model.addAttribute("userId", ((User)session.getAttribute("user")).getUserId());
+		return "forward:/chat/index2.jsp";
 	}
 	
 	
--- link/src/main/webapp/clubPost/getClubPostList.jsp
+++ link/src/main/webapp/clubPost/getClubPostList.jsp
@@ -164,7 +164,7 @@
 							<a href="/club/getClubMemberList">모임원</a>
 						</li>
 						<li data-group="infographics">
-							<a href="/club/chatRoomList">모임 채팅</a>
+							<a href="/clubPost/chatRoomList">모임 채팅</a>
 						</li>
 						<%-- <li data-group="infographics">
 							<a href="/clubPost/addPayView?clubNo=${ clubPostList[0].clubNo }">결제</a>
