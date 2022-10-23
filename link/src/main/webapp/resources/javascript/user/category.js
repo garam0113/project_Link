14b1c9c651e626cfd8ecc8a386f5c1e45c88b4a6
bitcamp <bitcamp@DESKTOP-B951KAA>
2022-10-24 00:24:11 +0900

    jeoungMin : 2022.10.24 00:22 회원관리 회원가입 오류(비밀번호 유효성, 주민등록번호 유효성) 수정 회원가입 CSS수정(버튼, 프로필사진추가) 프로필수정 CSS수정

--- link/src/main/webapp/resources/javascript/user/category.js
+++ link/src/main/webapp/resources/javascript/user/category.js
@@ -1,77 +1,78 @@
 $(function() {
 		$("#categryClick")
-				.on(
-						"click",
-						function() {
-							var category = $("#categorySelcet").val();
-
-							console.log("선택한 값 : " + category);
-
-							if (category != "선택"
-									&& $("#ca").children().length < 3) {
-								$("#categoryView1")
-										.html(
-												$("#ca")
-														.append(
-																"<div>"
-																		+ category
-				 														+ "  "
-																		+ "<button class='cate''>삭제</button></div>"));
-
-							}
-							var category1 = $($("#ca").children()[0]).text()
-									.replace("삭제", "").trim();
-							var category2 = $($("#ca").children()[1]).text()
-									.replace("삭제", "").trim();
-							var category3 = $($("#ca").children()[2]).text()
-									.replace("삭제", "").trim();
-
-							$("#category1").val(category1);
-							$("#category2").val(category2);
-							$("#category3").val(category3);
-
-							console.log($("#ca").children().length);
-
-							console.log(category1);
-							console.log(category2);
-							console.log(category3);
-
-							console.log("서버로 보낼 값 : " + $("#category1").val());
-							console.log("서버로 보낼 값 : " + $("#category2").val());
-							console.log("서버로 보낼 값 : " + $("#category3").val());
-
-							if ($("#ca").children().length == 3) {
-								swal.fire("최대3개까지 입니다.");
-								return;
-							}
-						});
-
-	});
-
-	$(function() {
-
-		$(document).on(
+			.on(
 				"click",
-				".cate",
 				function() {
+					var category = $("#categorySelcet").val();
 
-					console.log($(this).parent());
+					console.log("선택한 값 : " + category);
 
-					$(this).parent().remove();
+					if (category != "선택"
+						&& $("#ca").children().length < 3) {
+						$("#categoryView1")
+							.html(
+								$("#ca")
+									.append(
+										"<div style='margin-bottom: 10px;'>"
+										+ category
+										+ "  "
+										+ "<button style='border: hidden; background: #d9edf7; height: 30px; width: 50px;' class='cate'>삭제</button></div>"));
+						    
 
-					var category1 = $($("#ca").children()[0]).text().replace(
-							"삭제", "").trim();
-					var category2 = $($("#ca").children()[1]).text().replace(
-							"삭제", "").trim();
-					var category3 = $($("#ca").children()[2]).text().replace(
-							"삭제", "").trim();
+					}
+					var category1 = $($("#ca").children()[0]).text()
+					.replace("삭제", "").trim();
+				var category2 = $($("#ca").children()[1]).text()
+					.replace("삭제", "").trim();
+				var category3 = $($("#ca").children()[2]).text()
+					.replace("삭제", "").trim();
 
-					$("#category1").val(category1);
-					$("#category2").val(category2);
-					$("#category3").val(category3);
+				$("#category1").val(category1);
+				$("#category2").val(category2);
+				$("#category3").val(category3);
 
-					console.log("서버로 보낼 값 : " + $("#category1").val());
-					console.log("서버로 보낼 값 : " + $("#category2").val());
-					console.log("서버로 보낼 값 : " + $("#category3").val());
-				})
-	})
+				console.log($("#ca").children().length);
+
+				console.log(category1);
+				console.log(category2);
+				console.log(category3);
+
+				console.log("서버로 보낼 값 : " + $("#category1").val());
+				console.log("서버로 보낼 값 : " + $("#category2").val());
+				console.log("서버로 보낼 값 : " + $("#category3").val());
+
+				if ($("#ca").children().length == 3) {
+					swal.fire("최대3개까지 입니다.");
+					return;
+				}
+							});
+
+				});
+
+$(function() {
+
+	$(document).on(
+		"click",
+		".cate",
+		function() {
+
+			console.log($(this).parent());
+
+			$(this).parent().remove();
+
+			var category1 = $($("#ca").children()[0]).text().replace(
+				"삭제", "").trim();
+			var category2 = $($("#ca").children()[1]).text().replace(
+				"삭제", "").trim();
+			var category3 = $($("#ca").children()[2]).text().replace(
+				"삭제", "").trim();
+
+			$("#category1").val(category1);
+			$("#category2").val(category2);
+			$("#category3").val(category3);
+
+			console.log("서버로 보낼 값 : " + $("#category1").val());
+			console.log("서버로 보낼 값 : " + $("#category2").val());
+			console.log("서버로 보낼 값 : " + $("#category3").val());
+		})
+})
